ab4f34d18c7d9f3a46abf2862c0ef85a0affe40f
Altamas <creotove@gmail.com>
2024-03-14 16:52:06 +0530

    fixed: static removed

--- index.js
+++ index.js
@@ -10,34 +10,29 @@ import morgan from "morgan";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 connectDB();
 const app = express();
 
-app.use(express.json({ extended: true }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({  extended: true }));
+app.use(express.urlencoded({  extended: true }));
 app.use(morgan("dev"));
 app.use(cors());
 app.use(cookieParser());
+
 
 app.use("/admin", adminRoutes);
 app.use("/student", studentRoutes);
 app.use("/teacher", teacherRoutes);
 app.use("/user", userRoutes);
 app.use("/publisher", publisherRoutes);
-
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-}
+const html = `
+<body bgColor='black' style='color:white;font-size:5rem;display:flex;justify-content:center; align-items:center;' >Api is Running...</body>
+`;
+app.get("/", (req, res) => {
+  res.send(html);
+});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-  console.log(`Server Started on Port ${PORT}`);
+  console.log(`Server Started on Port ${PORT} \n http://localhost:${PORT}`);
 });
